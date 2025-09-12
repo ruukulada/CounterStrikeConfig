819afaddbd4af05e843bf1b7e29438c03bcd58fd
ruukulada <126130342+ruukulada@users.noreply.github.com>
2025-09-11 20:05:16 -0400

    Restructure

--- setup.bat
+++ setup.bat
@@ -1,7 +1,7 @@
 @echo off
 
 echo.
-echo Enter the installation directory, for example:
+echo Enter the CS2 installation directory, for example:
 echo C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive
 echo.
 set /p "Cs2Dir=Install location: "
@@ -9,11 +9,15 @@ set /p "Cs2Dir=Install location: "
 set "CfgDir=%Cs2Dir%\game\csgo\cfg"
 set "RepoDir=%~dp0"
 
-mklink /D "%CfgDir%\movement"         "%RepoDir%\movement"
-mklink    "%CfgDir%\autoexec.cfg"     "%RepoDir%\autoexec.cfg"
-mklink    "%CfgDir%\listenserver.cfg" "%RepoDir%\listenserver.cfg"
-mklink    "%CfgDir%\+jump.cfg"        "%RepoDir%\+jump.cfg"
-mklink    "%CfgDir%\-jump.cfg"        "%RepoDir%\-jump.cfg"
-
 echo.
+echo Copying ~: mklink /D "%CfgDir%\~" "%RepoDir%~"
+mklink /D "%CfgDir%\~" "%RepoDir%~"
+echo Copying MOVEMENT: mklink /D "%CfgDir%\MOVEMENT" "%RepoDir%MOVEMENT"
+mklink /D "%CfgDir%\MOVEMENT" "%RepoDir%MOVEMENT"
+echo Copying AUTOEXEC.cfg: mklink "%CfgDir%\AUTOEXEC.cfg" "%RepoDir%AUTOEXEC.cfg"
+mklink "%CfgDir%\AUTOEXEC.cfg" "%RepoDir%AUTOEXEC.cfg"
+echo Copying LISTENSERVER.cfg: mklink "%CfgDir%\LISTENSERVER.cfg" "%RepoDir%LISTENSERVER.cfg"
+mklink "%CfgDir%\LISTENSERVER.cfg" "%RepoDir%LISTENSERVER.cfg"
+echo.
+
 echo Config install complete
