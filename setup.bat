f9a8159a8dffa0b0a909b27fe86c4def8506f7bb
ruukulada <126130342+ruukulada@users.noreply.github.com>
2025-09-11 21:15:33 -0400

    No more rubyrain cfg

--- setup.bat
+++ setup.bat
@@ -1,23 +1,21 @@
 @echo off
-
-echo.
-echo Enter the CS2 installation directory, for example:
-echo C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive
-echo.
+echo. & echo.Enter the CS2 installation directory, for example:
+echo.C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive & echo.
 set /p "Cs2Dir=Install location: "
 
-set "CfgDir=%Cs2Dir%\game\csgo\cfg"
-set "RepoDir=%~dp0"
+set "CfgDir=%Cs2Dir%\game\csgo\cfg\"
+set "GitDir=%~dp0"
 
-echo.
-echo Copying ~: mklink /D "%CfgDir%\~" "%RepoDir%~"
-mklink /D "%CfgDir%\~" "%RepoDir%~"
-echo Copying MOVEMENT: mklink /D "%CfgDir%\MOVEMENT" "%RepoDir%MOVEMENT"
-mklink /D "%CfgDir%\MOVEMENT" "%RepoDir%MOVEMENT"
-echo Copying AUTOEXEC.cfg: mklink "%CfgDir%\AUTOEXEC.cfg" "%RepoDir%AUTOEXEC.cfg"
-mklink "%CfgDir%\AUTOEXEC.cfg" "%RepoDir%AUTOEXEC.cfg"
-echo Copying LISTENSERVER.cfg: mklink "%CfgDir%\LISTENSERVER.cfg" "%RepoDir%LISTENSERVER.cfg"
-mklink "%CfgDir%\LISTENSERVER.cfg" "%RepoDir%LISTENSERVER.cfg"
-echo.
+echo. & echo.Linking ~ dir: & echo.  %CfgDir%~
+echo     to & echo.  %GitDir%~
+mklink /D "%CfgDir%~" "%GitDir%~"
 
-echo Config install complete
+echo. & echo.Linking AUTOEXEC.cfg: & echo.  %CfgDir%AUTOEXEC.cfg
+echo     to & echo.  %GitDir%AUTOEXEC.cfg
+mklink "%CfgDir%AUTOEXEC.cfg" "%GitDir%AUTOEXEC.cfg"
+
+echo. & echo.Linking LISTENSERVER.cfg: & echo.  %CfgDir%LISTENSERVER.cfg
+echo     to & echo.  %GitDir%LISTENSERVER.cfg
+mklink "%CfgDir%LISTENSERVER.cfg" "%GitDir%LISTENSERVER.cfg" & echo.
+
+echo Config install complete & echo. & pause
